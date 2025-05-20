ddba1695d861a99f5c59f6838321d4f2e3a05c80
Phạm Tuấn Minh <153070983+TuanMinh1905@users.noreply.github.com>
2025-05-21 03:42:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WarehouseProProject.BLL;
+using WarehouseProProject.DAL.Repository;
+
+
+namespace WarehouseManagement
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var context = new WarehouseContext();
+            var userRepository = new UserRepository(context);
+
+            var userBLL = new UserBLL(userRepository);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Users(userBLL));
+        }
+    }
+}
